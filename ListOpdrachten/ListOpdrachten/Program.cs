e1095cc2ea454ab6330e3ca45c6d7f41b105d54b
TimothyKuijper <t.kuijper12@gmail.com>
2023-05-10 12:50:07 +0200

    update

---
+++ ListOpdrachten/ListOpdrachten/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ListOpdrachten
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> birds = new List<string>();
+        }
+    }
+}
